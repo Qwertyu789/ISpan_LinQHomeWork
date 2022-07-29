365a4074387d2d73d2e65677ffdc2c1de5b5b82e
仕旻 張 <michael19950629@gmail.com>
2022-07-29 17:19:53 +0800

    0729

--- LinqLabs/作業/Frm作業_3.Designer.cs
+++ LinqLabs/作業/Frm作業_3.Designer.cs
@@ -29,9 +29,9 @@ namespace LinqLabs
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button36 = new System.Windows.Forms.Button();
             this.button37 = new System.Windows.Forms.Button();
@@ -41,16 +41,16 @@ namespace LinqLabs
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(327, 28);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(429, 222);
             this.chart1.TabIndex = 151;
             this.chart1.Text = "chart1";
@@ -64,6 +64,7 @@ namespace LinqLabs
             this.button36.TabIndex = 150;
             this.button36.Text = "搜尋 班級學生成績";
             this.button36.UseVisualStyleBackColor = false;
+            this.button36.Click += new System.EventHandler(this.button36_Click);
             // 
             // button37
             // 
@@ -74,6 +75,7 @@ namespace LinqLabs
             this.button37.TabIndex = 149;
             this.button37.Text = "每個學生個人成績";
             this.button37.UseVisualStyleBackColor = false;
+            this.button37.Click += new System.EventHandler(this.button37_Click);
             // 
             // button33
             // 
--- LinqLabs/作業/Frm作業_3.cs
+++ LinqLabs/作業/Frm作業_3.cs
@@ -12,14 +12,56 @@ namespace LinqLabs
 {
     public partial class Frm作業_3 : Form
     {
+        List<Student> students_scores;
+        public class Student
+        {
+            public string Name { get; set; }
+            public string Class { get; set; }
+            public int Chi { get; set; }
+            public int Eng { get; internal set; }
+            public int Math { get; set; }
+            public string Gender { get; set; }
+        }
         public Frm作業_3()
         {
             InitializeComponent();
+            students_scores = new List<Student>()
+                                         {
+                                            new Student{ Name = "aaa", Class = "CS_101", Chi = 80, Eng = 80, Math = 50, Gender = "Male" },
+                                            new Student{ Name = "bbb", Class = "CS_102", Chi = 80, Eng = 80, Math = 100, Gender = "Male" },
+                                            new Student{ Name = "ccc", Class = "CS_101", Chi = 60, Eng = 50, Math = 75, Gender = "Female" },
+                                            new Student{ Name = "ddd", Class = "CS_102", Chi = 80, Eng = 70, Math = 85, Gender = "Female" },
+                                            new Student{ Name = "eee", Class = "CS_101", Chi = 80, Eng = 80, Math = 50, Gender = "Female" },
+                                            new Student{ Name = "fff", Class = "CS_102", Chi = 80, Eng = 80, Math = 80, Gender = "Female" },
+
+                                          };
         }
 
         private void button33_Click(object sender, EventArgs e)
         {
             // split=> 數學成績 分成 三群 '待加強'(60~69) '佳'(70~89) '優良'(90~100) 
+            var query = from m in students_scores
+                        group m by splitMath(m.Math) into g
+                        select new { StudentCount = g.Count(), g.Key };
+            this.chart1.DataSource = query;
+            //todo#2 作業3
+            //this.chart1.Series[0]=
+        }
+        string splitMath(int n)
+        {
+            if (n < 60) return "這個直接電死";
+            else if (n < 70) return "待加強";
+            else if (n < 90) return "佳";
+            else return "優良";
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
 
         }
     }
